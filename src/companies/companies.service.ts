edaa7d02126f7d90dba3c8af0a2803c3e36a0fe9
https://github.com/denisphamzero7/DAN <denisphamzero7>
2025-06-11 08:25:30 +0700

    get a company

--- src/companies/companies.service.ts
+++ src/companies/companies.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { CreateCompanyDto } from './dto/create-company.dto';
 import { UpdateCompanyDto } from './dto/update-company.dto';
 import { SoftDeleteModel } from 'soft-delete-plugin-mongoose';
@@ -6,6 +6,7 @@ import { Company,CompanyDocument } from './schemas/company.schema';
 import { InjectModel } from '@nestjs/mongoose';
 import { IUser } from 'src/users/user.interface';
 import aqp from 'api-query-params';
+import { Types } from 'mongoose';
 @Injectable()
 export class CompaniesService {
   constructor(@InjectModel(Company.name) 
@@ -25,11 +26,11 @@ export class CompaniesService {
   async findAll(currentPage: number, limit: number, qs: string) {
     const { filter, sort, projection, population } = aqp(qs);
   
-    // Xóa các trường không cần thiết nếu tồn tại
+
     delete filter.page;
     delete filter.limit;
   
-    // Thiết lập mặc định
+
     const page = currentPage || 1;
     const defaultLimit = limit || 10;
     const offset = (page - 1) * defaultLimit;
@@ -57,8 +58,11 @@ export class CompaniesService {
   }
   
 
-  findOne(id: number) {
-    return `This action returns a #${id} company`;
+  async findOne(id: string) {
+    if (!Types.ObjectId.isValid(id)) {
+      throw new BadRequestException('Invalid ID format');
+    }
+    return await this.companyModel.findOne({_id:id});
   }
 
   async update(id: string, updateCompanyDto: UpdateCompanyDto,user:IUser) {
