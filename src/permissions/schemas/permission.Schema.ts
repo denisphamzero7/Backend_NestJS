b5722a381726b6e8f00feace8ed93ee4e32a4bbe
https://github.com/denisphamzero7/DAN <denisphamzero7>
2025-06-13 08:40:54 +0700

    update schemas permission

--- src/permissions/schemas/permission.Schema.ts
+++ src/permissions/schemas/permission.Schema.ts
@@ -1,1 +1,44 @@
-export class Permission {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import mongoose, { HydratedDocument } from 'mongoose';
+
+export type PermissionDocument = HydratedDocument<Permission>;
+
+@Schema({ timestamps: true })
+export class Permission {
+  @Prop()
+  name: string;
+  
+  @Prop()
+  apiPath: string;
+
+  @Prop()
+  module: string;
+
+  
+  @Prop({ type: Object }) 
+  createBy: {
+    _id: mongoose.Schema.Types.ObjectId;
+    email: string;
+  };
+
+  @Prop({ type: Object }) 
+  updatedBy: {
+    _id: mongoose.Schema.Types.ObjectId;
+    email: string;
+  };
+
+  @Prop({ type: Object }) 
+  deleteBy: {
+    _id: mongoose.Schema.Types.ObjectId;
+    email: string;
+  };
+
+  @Prop()
+  deleteAt: Date;
+
+  @Prop()
+  isDeleted: boolean;
+
+}
+
+export const PermissionSchema = SchemaFactory.createForClass(Permission);
