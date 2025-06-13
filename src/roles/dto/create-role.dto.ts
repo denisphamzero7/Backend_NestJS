2511aad6149d8fc3b5f77b9f61b68f23494befde
https://github.com/denisphamzero7/DAN <denisphamzero7>
2025-06-13 13:11:55 +0700

    create DTO

--- src/roles/dto/create-role.dto.ts
+++ src/roles/dto/create-role.dto.ts
@@ -1,1 +1,20 @@
-export class CreateRoleDto {}
+import { IsArray, IsBoolean, IsMongoId, IsNotEmpty } from "class-validator";
+import mongoose from "mongoose";
+
+export class CreateRoleDto {
+    @IsNotEmpty({message:"Name is not empty"})
+    name: string;
+    
+    @IsBoolean({message:"isActive not empty"})
+    @IsNotEmpty({message:"Name is not empty"})
+    isActive:boolean; 
+
+    @IsNotEmpty({message:"description is not empty"})
+    description: string;
+    
+    @IsNotEmpty({message:"permission not empty"})
+    @IsMongoId({each:true,message:"each permissions is mongo object id"})
+    @IsArray({message:'permissions is array'})
+    permission:mongoose.Schema.Types.ObjectId[];
+
+}
---
+++ src/roles/schemas/role.schema.ts
@@ -0,0 +1,48 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import mongoose, { HydratedDocument } from 'mongoose';
+import { Permission } from 'src/permissions/schemas/permission.Schema';
+
+export type RoleDocument = HydratedDocument<Role>;
+
+@Schema({ timestamps: true })
+export class Role {
+  @Prop()
+  name: string;
+  
+  @Prop({type:[mongoose.Schema.Types.ObjectId],ref: Permission.name})
+  permission: Permission[];
+
+  @Prop()
+  description: string;
+
+
+  @Prop()
+  isActive:boolean;
+  
+  @Prop({ type: Object }) 
+  createBy: {
+    _id: mongoose.Schema.Types.ObjectId;
+    email: string;
+  };
+
+  @Prop({ type: Object }) 
+  updatedBy: {
+    _id: mongoose.Schema.Types.ObjectId;
+    email: string;
+  };
+
+  @Prop({ type: Object }) 
+  deleteBy: {
+    _id: mongoose.Schema.Types.ObjectId;
+    email: string;
+  };
+
+  @Prop()
+  deleteAt: Date;
+
+  @Prop()
+  isDeleted: boolean;
+
+}
+
+export const RoleSchema = SchemaFactory.createForClass(Role);
