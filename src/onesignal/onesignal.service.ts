1a068e5824a8bdd62139a417fcaee6655900b83d
ngochau <pnhau2001@gmail.com>
2026-08-06 08:29:44 +0700

    update code

--- src/onesignal/onesignal.service.ts
+++ src/onesignal/onesignal.service.ts
@@ -54,7 +54,7 @@ export class OnesignalService {
       );
       this.logger.log(`OneSignal notification sent to ${externalUserId}: ${response.data.id}`);
       return response.data;
-    } catch (error) {
+    } catch (error: any) {
       this.logger.error(`Error sending OneSignal notification to ${externalUserId}`, error.response?.data || error.message);
       throw error;
     }
